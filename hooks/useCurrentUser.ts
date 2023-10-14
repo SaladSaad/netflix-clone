b92567c0db27f40e4bfefd1599c96629f6839809
saadas <saadsheikh182000@gmail.com>
2023-10-13 21:10:50 -0700

    profiles page formatted and pseudo profile selection implemented

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,18 +1,18 @@
-import useSWR from "swr";
-import fetcher from "@/lib/fetcher";
+import useSWR from 'swr';
+import fetcher from '@/lib/fetcher';
 
-//swr is a vercil developed package for fatch data. like react query.
+//swr is a vercel developed package for fatch data. like react query.
 //the first time we fetch this /api/current, no matter where we use the useCurrentUser, it is not going to fetch it again if data exists. Don't need state management to manage user with this package.
 
 const useCurrentUser = () => {
-  const { data, error, isLoading, mutate } = useSWR("/api/current", fetcher);
+	const { data, error, isLoading, mutate } = useSWR('/api/current', fetcher);
 
-  return {
-    data,
-    error,
-    isLoading,
-    mutate,
-  };
+	return {
+		data,
+		error,
+		isLoading,
+		mutate,
+	};
 };
 
 export default useCurrentUser;
--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,5 +1,7 @@
 import { NextPageContext } from 'next';
+import { useRouter } from 'next/router';
 import { getSession } from 'next-auth/react';
+import useCurrentUser from '@/hooks/useCurrentUser';
 
 //if trying to access /profiles without having signed in, redirect to login page
 export async function getServerSideProps(context: NextPageContext) {
@@ -20,9 +22,32 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Profiles = () => {
+	const router = useRouter();
+	const { data: user } = useCurrentUser();
+
 	return (
-		<div>
-			<p className='text-white text-4xl'>Profiles</p>
+		<div className='flex items-center h-full justify-center'>
+			<div className='flex flex-col'>
+				<h1 className='text-white text-3xl text-center md:text-6xl'>
+					Who is watching?
+				</h1>
+				<div className='flex items-center justify-center gap-8 mt-10'>
+					<div onClick={() => router.push('/')}>
+						<div className='group flex-row w-44 mx-auto'>
+							<div className='w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden'>
+								<img
+									className='bg-blue-600'
+									src='/images/default-horse.png'
+									alt='Profile image'
+								/>
+							</div>
+							<div className='mt-4 text-gray-400 text-2xl text-center group-hover:text-white capitalize'>
+								{user?.name}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
